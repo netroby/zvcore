a2fcbdb883ceb8d809afebaf9593b42ac90a8006
ZhiFeng Hu <hufeng1987@gmail.com>
2015-08-10 05:56:55 +0800

    Remove lang
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- lang/en/sys.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-    'can_not_fetch_tpl_file'=>'Can not find The template file to load it!'
-    
-);
--- lang/gbk/sys.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-    'can_not_fetch_tpl_file'=>'不能加载指定的模板文件'
-    
-);
