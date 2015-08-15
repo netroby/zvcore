f56bab20a2a5d89d9ae4f974689a0274b4b49d0c
ZhiFeng Hu <hufeng1987@gmail.com>
2015-08-16 06:50:07 +0800

    Add namespace timetools

--- src/TimeTool.php
+++ src/TimeTool.php
@@ -1,14 +1,18 @@
-<?php 
+<?php
+namespace netroby\zvcore;
+
 /**
  * 时间类库，以后会添加和补充关于时间的处理函数。
  */
-class timefair {
-	/**
-	 * 返回昨天，今天，明天
-	 * @return Array 时间数组
-	 */
-    public static function fair() {
-    
+class timefair
+{
+    /**
+     * 返回昨天，今天，明天
+     * @return Array 时间数组
+     */
+    public static function fair()
+    {
+
         $timeMagic = array();
         $time = time();
         $today = date("F j,y", $time);
@@ -16,6 +20,6 @@ class timefair {
         $timeMagic['yesterday'] = $timeMagic['today'] - (3600 * 24);
         $timeMagic['tomorrow'] = $timeMagic['today'] + (3600 * 24);
         return $timeMagic;
-        
+
     }
 }
