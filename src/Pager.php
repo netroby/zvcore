668ef82aa32d013f6e198c5dc3fb611ebf6d8143
ZhiFeng Hu <hufeng1987@gmail.com>
2015-08-16 06:46:42 +0800

    Add namespace for pager

--- src/Pager.php
+++ src/Pager.php
@@ -1,5 +1,8 @@
-<?php 
-class pager {
+<?php
+namespace netroby\zvcore;
+
+class pager
+{
     /**
      * url的前缀
      * index-index
@@ -71,7 +74,7 @@ class pager {
      * @var string 分页导航HTML
      */
     public $show;
-    
+
     /**
      * 初始化
      * 设置变量，提示信息
@@ -79,37 +82,40 @@ class pager {
      * @param object $totalRows 总记录数
      * @param object $perPage 每页数据行数
      */
-    public function __construct($totalRows, $perPage = 20, $urlPrefix = null) {
-        if ( empty($totalRows)) {
+    public function __construct($totalRows, $perPage = 20, $urlPrefix = null)
+    {
+        if (empty($totalRows)) {
             throw new Exception('请指定总记录条数!');
         } else {
             //设定总记录条数
             $this->totalRows = $totalRows;
         }
-        
-        if ( empty($urlPrefix)) {
+
+        if (empty($urlPrefix)) {
             //取当前请求的URL地址
-            $this->urlPrefix = $_REQUEST['controller']."-".$_REQUEST['action'];
+            $this->urlPrefix = $_REQUEST['controller'] . "-" . $_REQUEST['action'];
         } else {
             $this->urlPrefix = $urlPrefix;
         }
-        
+
         //每页显示条数
         $this->perPage = $perPage;
-        
+
         //开始分页
         $this->setPager();
-        
+
     }
+
     /**
      * 分页计算
      * 限定最大页数，总页数等
      */
-    public function setPager() {
+    public function setPager()
+    {
         //计算总页数
         $this->totalPages = ceil($this->totalRows / $this->perPage);
         //获取当前页面数;
-        if ( empty($_REQUEST['page'])) {
+        if (empty($_REQUEST['page'])) {
             $pageNow = 1;
         } else {
             $pageNow = intval($_REQUEST['page']);
@@ -117,7 +123,7 @@ class pager {
         //检验页面数是否超出最大值范围和最小值范围
         $this->pageNow = $this->limitCheck($pageNow, 1, $this->totalPages);
 
-        
+
         //当前真实的页面数
         $pageTrue = ($this->pageNow) - 1;
         //计算最大许可数目
@@ -125,30 +131,32 @@ class pager {
         //检查真实页面数是否超出最大值
         $this->pageTrue = $this->limitCheck($pageTrue, 0, $maxPageTrue);
 
-        
+
         // 前一页
         $pagePrev = ($this->pageNow) - 1;
         $this->pagePrev = $this->limitCheck($pagePrev, 1, $this->totalPages);
-        
+
         //后一页
         $pageNext = ($this->pageNow) + 1;
         $this->pageNext = $this->limitCheck($pageNext, 1, $this->totalPages);
-        
+
         //第一页
         $this->pageFirst = 1;
         //末页
         $this->pageEnd = $this->totalPages;
-        
+
         //设定分页记录开始记录行数;
         $this->rowStart = $this->pageTrue * $this->perPage;
         //显示分页html代码
         $this->showPager();
     }
+
     /**
      * 分页导航
      * 生成分页html代码
      */
-    public function showPager() {
+    public function showPager()
+    {
         //分页显示起始页计算为当前页减2；
         $page_nav_start_num = $this->pageNow - 2;
 
@@ -156,7 +164,7 @@ class pager {
         if (($page_nav_start_num + 5) > ($this->totalPages)) {
             $page_nav_start_num = $this->totalPages - 4;
         }
-                //如果起始页小于1，置为1
+        //如果起始页小于1，置为1
         if ($page_nav_start_num < 1) {
             $page_nav_start_num = 1;
         }
@@ -173,21 +181,22 @@ class pager {
         }
         //构建显示开始
         $pager = '<ul class="pager">';
-        $pager .= '<li><a href="'.$this->urlPrefix.'-page-'.$this->pageFirst.'.html">|&lt;</a></li>';
-        $pager .= '<li><a href="'.$this->urlPrefix.'-page-'.$this->pagePrev.'.html">&lt;&lt;</a></li>';
+        $pager .= '<li><a href="' . $this->urlPrefix . '-page-' . $this->pageFirst . '.html">|&lt;</a></li>';
+        $pager .= '<li><a href="' . $this->urlPrefix . '-page-' . $this->pagePrev . '.html">&lt;&lt;</a></li>';
         //构建分页显示
         for ($i = $page_nav_start_num; $i <= $page_nav_end_num; $i++) {
             if ($i == $this->pageNow) {
-                $pager .= '<li><a href="'.$this->urlPrefix.'-page-'.$i.'.html" class="pageNow">'.$i.'</a></li>';
+                $pager .= '<li><a href="' . $this->urlPrefix . '-page-' . $i . '.html" class="pageNow">' . $i . '</a></li>';
             } else {
-                $pager .= '<li><a href="'.$this->urlPrefix.'-page-'.$i.'.html">'.$i.'</a></li>';
+                $pager .= '<li><a href="' . $this->urlPrefix . '-page-' . $i . '.html">' . $i . '</a></li>';
             }
         }
-        $pager .= '<li><a href="'.$this->urlPrefix.'-page-'.$this->pageNext.'.html">&gt;&gt;</a></li>';
-        $pager .= '<li><a href="'.$this->urlPrefix.'-page-'.$this->pageEnd.'.html">&gt;|</a></li>';
-        $pager .= '<li><a href="#"><span style="color:red">'.$this->pageNow.'</span>/'.$this->totalPages.'</a></li></ul>';
+        $pager .= '<li><a href="' . $this->urlPrefix . '-page-' . $this->pageNext . '.html">&gt;&gt;</a></li>';
+        $pager .= '<li><a href="' . $this->urlPrefix . '-page-' . $this->pageEnd . '.html">&gt;|</a></li>';
+        $pager .= '<li><a href="#"><span style="color:red">' . $this->pageNow . '</span>/' . $this->totalPages . '</a></li></ul>';
         $this->show = $pager;
     }
+
     /**
      * 限定最大值和最小值
      * 最大不超过最大值，最小不低于最小值
@@ -196,10 +205,11 @@ class pager {
      * @param object $min 最小值
      * @param object $max 最大值
      */
-    public function limitCheck($val, $min, $max) {
+    public function limitCheck($val, $min, $max)
+    {
         if ($val < $min) {
             return $val = $min;
-            
+
         } elseif ($val > $max) {
             return $val = $max;
         } else {
